dd0e739bbcd8c68c03d09a8829802616ea8134e1
Artem Kozaev <artemkozaev@gmail.com>
2022-12-10 21:22:14 +0300

    add debounce

--- autolike.js
+++ autolike.js
@@ -7,24 +7,41 @@
 /// <reference path="../globals.d.ts" />
 
 (function Autolike() {
-  if (!document.querySelector(".control-button-heart") || !Spicetify.Player) {
+  const player = Spicetify.Player;
+
+  if (!getLikeButton() || !player) {
     setTimeout(Autolike, 100);
     return;
   }
 
-  Spicetify.Player.addEventListener("onprogress", watchChange);
+  player.addEventListener("onprogress", watchChange);
 
-  function watchChange(v) {
-    const heart = document.querySelector(".control-button-heart");
+  const DEBOUNCE_TIME = 3000;
+  const LIKE_THRESHOLD = 0.95;
+  let debouncing = 0;
+
+  function watchChange(event) {
+    // run only once per 3 seconds
+    if (debouncing) {
+      if (event.timeStamp - debouncing > DEBOUNCE_TIME) debouncing = 0;
+      return;
+    }
+
+    debouncing = event.timeStamp;
+
+    const heart = getLikeButton();
 
     if (
-      Spicetify.Player.isPlaying() &&
-      Spicetify.Player.getProgressPercent() > 0.95 &&
-      heart.ariaChecked === "false"
-    ) {
-      Spicetify.showNotification("Will be autoliked...");
+      heart.ariaChecked === "true" ||
+      !player.isPlaying() ||
+      player.getProgressPercent() < LIKE_THRESHOLD
+    )
+      return;
 
-      heart.click();
-    }
+    heart.click();
+  }
+
+  function getLikeButton() {
+    return document.querySelector(".control-button-heart");
   }
 })();
